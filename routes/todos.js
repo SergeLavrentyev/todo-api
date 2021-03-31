d4e7d792fec0be15cec2bb79103ea6fd217d65c9
SergeLavrentev <bert679@yandex.ru>
2021-03-31 19:00:48 +0400

    add routes

---
+++ routes/todos.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.delete("/", (req, res) => {});
+
+router.patch("/", (req, res) => {});
